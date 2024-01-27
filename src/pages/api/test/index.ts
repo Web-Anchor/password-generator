4e0a6b949e604ed0f23678400dd139b7ac415822
Ed <128392461+edtheanchor@users.noreply.github.com>
2024-01-27 17:03:33 +0000

    New commit message: add api test routes with examples

---
+++ src/pages/api/test/index.ts
@@ -0,0 +1,66 @@
+import type { APIRoute, APIContext } from 'astro'
+
+export const GET: APIRoute = async ({
+  params,
+  request,
+}: APIContext): Promise<Response> => {
+  const response: any = {
+    message: 'This is a GET request',
+    params: params,
+  }
+
+  return new Response(JSON.stringify(response), {
+    status: 200,
+    statusText: 'OK',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Custom-Header': 'foo', // custom header example
+    },
+  })
+}
+
+export const POST: APIRoute = async ({
+  params,
+  request,
+}: APIContext): Promise<Response> => {
+  const response: any = {
+    message: 'This is a POST request',
+    params: params,
+  }
+
+  return new Response(JSON.stringify(response), {
+    status: 200,
+    statusText: 'OK',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Custom-Header': 'foo', // custom header example
+    },
+  })
+}
+
+export const PUT: APIRoute = async ({
+  params,
+  request,
+  url,
+}: APIContext): Promise<Response> => {
+  const { id } = params
+  // get searchParams from url and get id
+  const searchParams = new URLSearchParams(url.search)
+  const searchId = searchParams.get('id')
+  console.log('⭐️ ', params, url, searchId)
+
+  if (!searchId) {
+    return new Response(null, {
+      status: 404,
+      statusText: 'Not found',
+    })
+  }
+
+  // redirect to the home page return
+  return new Response(null, {
+    status: 302, // Set the redirect status code (302 for temporary redirect)
+    headers: {
+      Location: 'http://localhost:4321/', // Set the Location header to the redirect URL
+    },
+  })
+}
