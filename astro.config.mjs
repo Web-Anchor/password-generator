5be15326a6731452ce88a2320d18b509aecb5f6b
Ed <128392461+edtheanchor@users.noreply.github.com>
2024-01-27 15:50:07 +0000

    New commit message: add vercel adaapter & enable ssr mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless'
 
 export default defineConfig({
   // ...
   integrations: [tailwind()],
-});
+  // --------------------------------------------------------------------------------
+  // 📌  SSR for page generation & vercel host adaptor
+  // --------------------------------------------------------------------------------
+  output: 'server',
+  adapter: vercel(),
+})
