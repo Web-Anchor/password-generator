a42510b4862a6391153a4affb891d8be889aebbd
Ed <128392461+edtheanchor@users.noreply.github.com>
2024-01-30 23:31:25 +0000

    New commit message: add base input compoennt and snags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import vercel from '@astrojs/vercel/serverless'
+import react from '@astrojs/react'
 
 export default defineConfig({
   // ...
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
   // --------------------------------------------------------------------------------
   // 📌  SSR for page generation & vercel host adaptor
   // --------------------------------------------------------------------------------
---
+++ src/components/input.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+export function Input() {
+  return (
+    <div className="flex items-start space-x-4 lg:px-40">
+      <div className="flex-shrink-0">
+        <img
+          className="inline-block h-10 w-10 rounded-full"
+          src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+          alt=""
+        />
+      </div>
+      <div className="min-w-0 flex-1">
+        <form action="#" className="relative">
+          <div className="overflow-hidden rounded-lg shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-indigo-600">
+            <label htmlFor="comment" className="sr-only">
+              Add your comment
+            </label>
+            <textarea
+              name="comment"
+              id="comment"
+              className="block w-full resize-none border-0 bg-transparent py-1.5 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+              placeholder="Add your request..."
+            ></textarea>
+
+            <div className="py-2" aria-hidden="true">
+              <div className="py-px">
+                <div className="h-9"></div>
+              </div>
+            </div>
+          </div>
+
+          <div className="absolute inset-x-0 bottom-0 flex justify-between py-2 pl-3 pr-2">
+            <div className="flex-shrink-0 ml-auto">
+              <button
+                type="submit"
+                className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Post
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
