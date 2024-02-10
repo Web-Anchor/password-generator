ef4abb7249c0e0e6af52b74eff140b13839f6e39
Ed <128392461+edtheanchor@users.noreply.github.com>
2024-02-10 08:22:26 +0000

    New commit message: add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import vercel from '@astrojs/vercel/serverless'
 import react from '@astrojs/react'
+import netlify from '@astrojs/netlify'
 
 export default defineConfig({
   // ...
@@ -17,5 +18,6 @@ export default defineConfig({
   // 📌  SSR for page generation & vercel host adaptor
   // --------------------------------------------------------------------------------
   output: 'server',
+  adapter: netlify(),
   adapter: vercel(),
 })
